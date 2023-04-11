3306fe7a40192da8d435239fa6b14b3bcebe1805
dominici94 <mattiadominici94@gmail.com>
2023-04-11 12:33:36 +0200

    utilizzo useState()

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpenses/NewExpense";
 
 function App() {
   const expenses = [
@@ -25,8 +26,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <p>This is also visible</p>
+      <NewExpense></NewExpense>
       <Expenses expenses={expenses}></Expenses>
     </div>
   );
