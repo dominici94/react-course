8cb16428c3cb8a39501fe53eb8a3cc7c23692eb7
dominici94 <mattiadominici94@gmail.com>
2023-05-10 17:58:36 +0200

    next js project 2 init

--- 23-Nextjs_first_project/pages/news/index.js
+++ 23-Nextjs_first_project/pages/news/index.js
@@ -1,5 +1,15 @@
+import Link from "next/link";
+
 function NewsPage() {
-  return <h1>The news page</h1>;
+  return (
+    <>
+      <h1>The news page</h1>
+      <ul>
+        <Link href="/news/nextjs-framework">NextJs framework</Link>
+        <li>Something else</li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
