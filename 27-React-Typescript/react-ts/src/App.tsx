42f06870e5d9ed9a5e1b915ad3a1cffa0978daf7
dominici94 <mattiadominici94@gmail.com>
2023-05-16 18:21:58 +0200

    todo list ts

--- 27-React-Typescript/react-ts/src/App.tsx
+++ 27-React-Typescript/react-ts/src/App.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todotext: string) => {
+    const newTodo = new Todo(todotext);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
+  const removeTodoHandler = (todoId: string) => {
+    // const [removingTodo] = todos.filter((todo) => todo.id === id);
+
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
     <div>
-      <Todos items={["pippo", "Pluto", "Learn React", "Learn Typescript"]} />
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   );
 }
