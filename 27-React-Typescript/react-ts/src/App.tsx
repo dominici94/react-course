570103964858015c3a2f1c9e3afd881103d9a0b9
dominici94 <mattiadominici94@gmail.com>
2023-05-16 19:40:27 +0200

    useContext ts react

--- 27-React-Typescript/react-ts/src/App.tsx
+++ 27-React-Typescript/react-ts/src/App.tsx
@@ -1,33 +1,13 @@
-import { useState } from "react";
-
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
+import TodosContextProvider from "./store/todos-context";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const addTodoHandler = (todotext: string) => {
-    const newTodo = new Todo(todotext);
-
-    setTodos((prevTodos) => {
-      return prevTodos.concat(newTodo);
-    });
-  };
-
-  const removeTodoHandler = (todoId: string) => {
-    // const [removingTodo] = todos.filter((todo) => todo.id === id);
-
-    setTodos((prevTodos) => {
-      return prevTodos.filter((todo) => todo.id !== todoId);
-    });
-  };
-
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-    </div>
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
   );
 }
 
